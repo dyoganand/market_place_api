0c804f41e7becf3f086fdaf472a3e926e113ff11
Yoga <yoganand19@gmail.com>
2016-04-05 16:43:14 +0530

    Initialize the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MarketPlaceApi::Application.config.secret_key_base = '53a288204b7d297889a4407a39e194d89562b01f1340a5f50d5a3822f5ae9d734a4d662670abdf13c0716a28a6a5eb6bf54ffd1dec9eb284c9d838fcfcd07272'
